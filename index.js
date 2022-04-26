c0b15b4e15fecd49e1c778dee21f21fbfd23aba5
Ayoub Taouarda <ayoubtrdone@gmail.com>
2022-04-26 21:50:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+require("./src/mailer");
+const express = require("express");
+
+const app = express();
+
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+const sgMail = require("@sendgrid/mail");
+
+app.post("/notify-self", async (req, res) => {
+  const { message, title } = req.body;
+  try {
+    await sgMail.send({
+      from: process.env.MAIL_SENDER,
+      to: process.env.MAIL_RECEIVER,
+      subject: title,
+      html: message,
+    });
+    res.send({
+      status: "success",
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(500).send({ error: e });
+  }
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("The server has started");
+});
